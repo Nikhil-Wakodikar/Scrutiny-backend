924717ca74bd8f978cfd3f6b6a55375a89394032
Akash <akash@Akashs-MacBook-Air.local>
2024-11-08 10:37:19 +0530

    validation update

--- src/routes/votes-account.route.js
+++ src/routes/votes-account.route.js
@@ -12,7 +12,7 @@ router
   .post(
     [
       uploadVotersAccount.single("file"),
-      votesAccountValidation.createVotesAccountWithoutAuth,
+      validate(votesAccountValidation.createVotesAccountWithoutAuth),
     ],
     votesAccountController.createVotesAccountWithoutAuth
   );
